55ba5f96afb617755c41ef2162d256a69446219f
ridewithoutstomach <bernd@casa-eller.de>
2025-07-31 19:51:17 +0200

    dev_3.34

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ from PySide6.QtCore import QSettings
 # 1) Versions-Konfiguration & Modus
 ##############################################################################
 
-APP_VERSION = "3.33"
+APP_VERSION = "3.34"
 
 
 #SERVER_VERSION_CHECK_ONLY = False
