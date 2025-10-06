7e64ba1be6200e240b4e576295cfba022a36d1cb
ridewithoutstomach <bernd@casa-eller.de>
2025-10-06 12:17:03 +0200

    config

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ from PySide6.QtCore import QSettings
 # 1) Versions-Konfiguration & Modus
 ##############################################################################
 
-APP_VERSION = "3.35"
+APP_VERSION = "3.36"
 
 
 #SERVER_VERSION_CHECK_ONLY = False
